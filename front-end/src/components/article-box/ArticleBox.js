7a5ae225e1f501b003321df6a31aaa0f573a8751
Alirezak77 <kangavari9@gmail.com>
2025-10-31 03:14:27 -0700

    show last published articles

--- front-end/src/components/article-box/ArticleBox.js
+++ front-end/src/components/article-box/ArticleBox.js
@@ -1,29 +1,30 @@
 import React from "react";
 import "./ArticleBox.css";
+import { Link } from "react-router-dom";
 
-export default function ArticleBox({title , desc , img}) {
+export default function ArticleBox({title , desc , img , shortName}) {
   return (
     <div class="col-4">
       <div class="article-card">
         <div class="article-card__header">
-          <a href="w" class="article-card__link-img">
+          <Link to={shortName} class="article-card__link-img">
             <img
               src={img}
               class="article-card__img"
               alt="Article Cover"
             />
-          </a>
+          </Link>
         </div>
         <div class="article-card__content">
-          <a href="w" class="article-card__link">
+          <Link to={shortName} class="article-card__link">
             {title}
-          </a>
+          </Link>
           <p class="article-card__text">
             {desc}
           </p>
-          <a href="w" class="article-card__btn">
+          <Link to={shortName} class="article-card__btn">
             بیشتر بخوانید
-          </a>
+          </Link>
         </div>
       </div>
     </div>
--- front-end/src/components/last-articles/LasrArticles.js
+++ front-end/src/components/last-articles/LasrArticles.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React ,{useState , useEffect}from "react";
 import "./LasrArticles.css";
 import CoursesHeader from "../coureses-header/CoursesHeader";
 import ArticleBox from "../article-box/ArticleBox";
 export default function LasrArticles() {
+
+    const [allArticles, setAllArticles]= useState([])
+    useEffect(()=>{
+      const localStorageData= JSON.parse(localStorage.getItem('user'))
+      fetch(`http://localhost:4000/v1/articles`,{
+        method:'GET',
+        headers:{Authorization: `Bearer ${localStorageData === null ? null : localStorageData}`,}
+      }).then(res=>res.json()).then(article=>{
+        console.log(article);
+        setAllArticles(article)
+        
+      })
+    },[])
   return (
     <section class="articles">
       <div class="container">
@@ -13,9 +26,10 @@ export default function LasrArticles() {
         />
         <div class="articles__content">
           <div class="row">
-            <ArticleBox title={'نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون'} desc={'زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع...'} img={'images/blog/3.jpeg'}/>
-            <ArticleBox title={'نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون'} desc={'زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع...'} img={'images/blog/3.jpeg'}/>
-            <ArticleBox title={'نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون'} desc={'زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع...'} img={'images/blog/3.jpeg'}/>
+            {allArticles.slice(0,3).map((article)=>{
+              return <ArticleBox title={article.title} desc={article.description} img={article.cover} shortName={article.shortName}/>
+            })}
+            
           </div>
         </div>
       </div>
