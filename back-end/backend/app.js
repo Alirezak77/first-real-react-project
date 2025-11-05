f71d1107bc400fb7e6221e4871985b999ceab9cb
Alirezak77 <kangavari9@gmail.com>
2025-11-05 07:52:23 -0800

    Backend update to add search functionality

--- back-end/backend/app.js
+++ back-end/backend/app.js
@@ -6,12 +6,14 @@ const courseRoutes = require("./routes/v1/course");
 const menuRoutes = require("./routes/v1/menu");
 const categoryRoutes = require("./routes/v1/category");
 const articlesRoutes = require("./routes/v1/article");
-const bodyParser = require('body-parser')
+const commentsRoutes = require("./routes/v1/comment");
+const searchRoutes = require("./routes/v1/search");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
@@ -19,14 +21,15 @@ app.use(
   express.static(path.join(__dirname, "public", "course", "covers"))
 );
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/v1/auth", authRoutes);
 app.use("/v1/courses", courseRoutes);
 app.use("/v1/menus", menuRoutes);
 app.use("/v1/category", categoryRoutes);
 app.use("/v1/articles", articlesRoutes);
+app.use("/v1/comments", commentsRoutes);
+app.use("/v1/search", searchRoutes);
 
 module.exports = app;
