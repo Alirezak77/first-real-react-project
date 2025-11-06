28cb3e51772371a97c0810ee74bc6a599066d523
Alirezak77 <kangavari9@gmail.com>
2025-11-06 07:27:47 -0800

    data table component for admin panel

--- front-end/src/pages/admin-panel/Index.js
+++ front-end/src/pages/admin-panel/Index.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import './Index.css'
-import { Outlet } from 'react-router-dom'
-import Sidebar from '../../components/admin-components/sidebar/SideBar'
-import Topbar from '../../components/admin-components/top-bar/TopBar'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../../components/admin-components/sidebar/SideBar";
+import Topbar from "../../components/admin-components/top-bar/TopBar";
+
+import "./Index.css";
 
 export default function index() {
   return (
     <>
-    <div id='content'>
-      <Sidebar/>
-      <div id='home' className='col-10'>
-        <Topbar/>
-      </div>
-    </div>
+      <div id="content">
+        <Sidebar />
 
-      <Outlet/>
+        <div id="home" class="col-10">
+          <Topbar />
+
+          <div class="container-fluid" id="home-content">
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
