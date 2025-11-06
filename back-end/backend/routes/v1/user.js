c8432ea79e959b5f3470885a9f7e66fee617aff7
Alirezak77 <kangavari9@gmail.com>
2025-11-06 08:41:16 -0800

    remove user handling

--- back-end/backend/routes/v1/user.js
+++ back-end/backend/routes/v1/user.js
@@ -19,24 +19,25 @@ router
   .get(isAuthenticated, isAdminMiddleware, userController.getAll);
 
 router
-  .route(":/id")
+  .route("/:id")
   .delete(isAuthenticated, isAdminMiddleware, userController.removeUser);
 
-// router
-//   .route("/ban/:id")
-//   .put(isAuthenticated, isAdminMiddleware, userController.banUser);
-
-// router
-//   .route("/:id/sessions")
-//   .post(isAdminMiddleware, courseController.createSession);
-
-// router.route('/presell').get(courseController.getAll)
-// router.route('/popular').get(courseController.getAll)
-
-// router.route("/:shortName").post(loginUser, courseController.getOne);
-
-// router.route("/:id/register").post(courseController.register);
-
-// router.route('/category/:categoryName').get(courseController.getCategoryCourses)
-
-module.exports = router;
+  
+  module.exports = router;
+  
+  // router
+  //   .route("/ban/:id")
+  //   .put(isAuthenticated, isAdminMiddleware, userController.banUser);
+  
+  // router
+  //   .route("/:id/sessions")
+  //   .post(isAdminMiddleware, courseController.createSession);
+  
+  // router.route('/presell').get(courseController.getAll)
+  // router.route('/popular').get(courseController.getAll)
+  
+  // router.route("/:shortName").post(loginUser, courseController.getOne);
+  
+  // router.route("/:id/register").post(courseController.register);
+  
+  // router.route('/category/:categoryName').get(courseController.getCategoryCourses)
--- front-end/src/pages/admin-panel/users/Users.js
+++ front-end/src/pages/admin-panel/users/Users.js
@@ -1,17 +1,50 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "../../../components/admin-components/DataTable/DataTable";
+import swal from "sweetalert";
 
 export default function Users() {
-  const [users, setUsers]=useState([])
-  useEffect(()=>{
-    const localStorageData = JSON.parse(localStorage.getItem('user'))
-    fetch(`http://localhost:4000/v1/users` ,{
+  const [users, setUsers] = useState([]);
+  const localStorageData = JSON.parse(localStorage.getItem("user"));
+
+  function getAllUsers() {
+    fetch(`http://localhost:4000/v1/users`, {
       headers: {
-        Authorization : `Bearer ${localStorageData}`
+        Authorization: `Bearer ${localStorageData}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((users) => setUsers(users));
+  }
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  const deleteUser = (userID) => {
+    swal({
+      title: "آیا از حذف کاربر اطمینان دارید؟",
+      icon: "warning",
+      buttons: ["خیر", "بله"],
+    }).then((result) => {
+      if (result) {
+        fetch(`http://localhost:4000/v1/users/${userID}`, {
+          method: "DELETE",
+          headers: { Authorization: `Bearer ${localStorageData}` },
+        }).then((res) => {
+          if (res.ok) {
+            swal({
+              title: "کاربر با موفقیت حذف شد",
+              icon: "success",
+              buttons: "تایید",
+            }).then(() => {
+              getAllUsers();
+            });
+          }
+        });
       }
-    }).then(res=>res.json()).then(users=>setUsers(users)
-    )
-  },[])
+    });
+  };
+
   return (
     <>
       <DataTable title="کاربران">
@@ -20,7 +53,7 @@ export default function Users() {
             <tr>
               <th>شناسه</th>
               <th>نام و نام خانوادگی</th>
-              
+
               <th>ایمیل</th>
               <th>ویرایش</th>
               <th>حذف</th>
@@ -28,29 +61,34 @@ export default function Users() {
             </tr>
           </thead>
           <tbody>
-            {users.map((user , index)=>{
-              return <tr>
-              <td>{index + 1}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>
-                <button type="button" class="btn btn-primary edit-btn">
-                  ویرایش
-                </button>
-              </td>
-              <td>
-                <button type="button" class="btn btn-danger delete-btn">
-                  حذف
-                </button>
-              </td>
-              <td>
-                <button type="button" class="btn btn-danger delete-btn">
-                  مسدود کردن
-                </button>
-              </td>
-            </tr>
+            {users.map((user, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <button type="button" class="btn btn-primary edit-btn">
+                      ویرایش
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-danger delete-btn"
+                      onClick={() => deleteUser(user._id)}
+                    >
+                      حذف
+                    </button>
+                  </td>
+                  <td>
+                    <button type="button" class="btn btn-danger delete-btn">
+                      مسدود کردن
+                    </button>
+                  </td>
+                </tr>
+              );
             })}
-            
           </tbody>
         </table>
       </DataTable>
