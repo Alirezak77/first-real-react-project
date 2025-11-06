893d8565f0de547048550f1ff4d635c125b1b083
Alirezak77 <kangavari9@gmail.com>
2025-11-05 23:37:40 -0800

    creat a top bar for the admin panel

--- front-end/src/pages/admin-panel/Index.js
+++ front-end/src/pages/admin-panel/Index.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import './Index.css'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../../components/admin-components/sidebar/SideBar'
+import Topbar from '../../components/admin-components/top-bar/TopBar'
 
 export default function index() {
   return (
     <>
     <div id='content'>
       <Sidebar/>
+      <div id='home' className='col-10'>
+        <Topbar/>
+      </div>
     </div>
+
       <Outlet/>
     </>
   )
