70087092b70643e29c8ff529580bc8d8193dae28
Alirezak77 <kangavari9@gmail.com>
2025-11-06 02:35:20 -0800

    backend update to display notification in the admin panel

--- back-end/backend/controllers/v1/auth.js
+++ back-end/backend/controllers/v1/auth.js
@@ -1,13 +1,14 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const userModel = require('../../models/user');
-const courseUserModel = require('../../models/course-user');
-const registerValidator = require('../../validators/v1/register');
+const userModel = require("../../models/user");
+const courseUserModel = require("../../models/course-user");
+const notificationsModel = require("../../models/notification");
+const registerValidator = require("../../validators/v1/register");
 
 exports.register = async (req, res) => {
   console.log(req.body);
-  
+
   const validationResult = registerValidator(req.body);
   if (validationResult != true) return res.status(422).json(validationResult);
   const { username, password, name, email } = req.body;
@@ -20,7 +21,7 @@ exports.register = async (req, res) => {
 
   if (isUserExists) {
     return res.status(409).json({
-      message: 'username or email is duplicate.',
+      message: "username or email is duplicate.",
     });
   }
 
@@ -31,15 +32,15 @@ exports.register = async (req, res) => {
     username,
     name,
     password: hashedPassword,
-    role: countOfRegisteredUser > 0 ? 'USER' : 'ADMIN',
+    role: countOfRegisteredUser > 0 ? "USER" : "ADMIN",
   });
 
   const userObject = user.toObject();
 
-  Reflect.deleteProperty(userObject, 'password');
+  Reflect.deleteProperty(userObject, "password");
 
   const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: '1 day',
+    expiresIn: "1 day",
   });
 
   return res.status(201).json({ user: userObject, accessToken });
@@ -53,17 +54,17 @@ exports.login = async (req, res) => {
   });
 
   if (!user) {
-    return res.status(401).json('there is no user with this email or username');
+    return res.status(401).json("there is no user with this email or username");
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
 
   if (!isPasswordValid) {
-    return res.status(401).json({ message: 'password is not correct' });
+    return res.status(401).json({ message: "password is not correct" });
   }
 
   const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: '30 day',
+    expiresIn: "30 day",
   });
 
   return res.json({ accessToken });
@@ -72,7 +73,7 @@ exports.login = async (req, res) => {
 exports.getMe = async (req, res) => {
   const userCourses = await courseUserModel
     .find({ user: req.user._id })
-    .populate('course');
+    .populate("course");
 
   const courses = [];
 
@@ -80,5 +81,99 @@ exports.getMe = async (req, res) => {
     courses.push(userCourse.course);
   }
 
-  return res.json({ ...req.user, courses });
+  const adminNotifications = await notificationsModel
+    .find({ admin: req.user._id })
+
+  const notifications = [];
+  
+  for (const adminNotification of adminNotifications) {
+    notifications.push(adminNotification.msg);
+  }
+  
+  return res.json({ ...req.user, courses, notifications });
 };
+
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+
+// const userModel = require('../../models/user');
+// const courseUserModel = require('../../models/course-user');
+// const registerValidator = require('../../validators/v1/register');
+
+// exports.register = async (req, res) => {
+//   console.log(req.body);
+  
+//   const validationResult = registerValidator(req.body);
+//   if (validationResult != true) return res.status(422).json(validationResult);
+//   const { username, password, name, email } = req.body;
+
+//   const isUserExists = await userModel.findOne({
+//     $or: [{ username }, { email }],
+//   });
+
+//   const countOfRegisteredUser = await userModel.count();
+
+//   if (isUserExists) {
+//     return res.status(409).json({
+//       message: 'username or email is duplicate.',
+//     });
+//   }
+
+//   const hashedPassword = await bcrypt.hash(password, 12);
+
+//   const user = await userModel.create({
+//     email,
+//     username,
+//     name,
+//     password: hashedPassword,
+//     role: countOfRegisteredUser > 0 ? 'USER' : 'ADMIN',
+//   });
+
+//   const userObject = user.toObject();
+
+//   Reflect.deleteProperty(userObject, 'password');
+
+//   const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+//     expiresIn: '1 day',
+//   });
+
+//   return res.status(201).json({ user: userObject, accessToken });
+// };
+
+// exports.login = async (req, res) => {
+//   const { identifier, password } = req.body;
+
+//   const user = await userModel.findOne({
+//     $or: [{ email: identifier }, { username: identifier }],
+//   });
+
+//   if (!user) {
+//     return res.status(401).json('there is no user with this email or username');
+//   }
+
+//   const isPasswordValid = await bcrypt.compare(password, user.password);
+
+//   if (!isPasswordValid) {
+//     return res.status(401).json({ message: 'password is not correct' });
+//   }
+
+//   const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+//     expiresIn: '30 day',
+//   });
+
+//   return res.json({ accessToken });
+// };
+
+// exports.getMe = async (req, res) => {
+//   const userCourses = await courseUserModel
+//     .find({ user: req.user._id })
+//     .populate('course');
+
+//   const courses = [];
+
+//   for (const userCourse of userCourses) {
+//     courses.push(userCourse.course);
+//   }
+
+//   return res.json({ ...req.user, courses });
+// };
