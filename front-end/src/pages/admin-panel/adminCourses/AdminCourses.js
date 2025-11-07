4eed88ec9fd2bba7c9ae9ae96042cc629fe58366
Alirezak77 <kangavari9@gmail.com>
2025-11-07 05:56:57 -0800

    delete course from admin panel

--- front-end/src/pages/admin-panel/adminCourses/AdminCourses.js
+++ front-end/src/pages/admin-panel/adminCourses/AdminCourses.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "../../../components/admin-components/DataTable/DataTable";
+import swal from "sweetalert";
 
 export default function AdminCourses() {
   const [courses , setCourses] = useState([])
+  const localStorageData= JSON.parse(localStorage.getItem('user'))
 
   useEffect(()=>{
-    const localStorageData= JSON.parse(localStorage.getItem('user'))
+    getAllCourses()
+  },[])
+
+  function getAllCourses(){
     fetch(`http://localhost:4000/v1/courses`,{
       headers:{ Authorization : `Bearer ${localStorageData}`} 
     }).then(res=>res.json()).then(courses=>{
@@ -14,7 +19,38 @@ export default function AdminCourses() {
 
       
     })
-  },[])
+  }
+
+  const deleteCourse =(courseID)=>{
+    swal({
+      title: 'آیا از حذف این دوره اطمینان دارید؟',
+      icon : 'warning',
+      buttons: ['خیر','بله']
+    }).then(result=>{
+      if(result){
+        fetch(`http://localhost:4000/v1/courses/${courseID}`,{
+          method: 'DELETE',
+          headers: {'Authorization': `Bearer ${localStorageData}`}
+        }).then(res=>{
+          if(res.ok){
+            swal({
+              title:'دوره با موفقیت حذف شد',
+              icon:'success',
+              buttons:'تایید'
+            }).then(()=>{
+              getAllCourses()
+            })
+          }else{
+            swal({
+              title:'در حذف دوره مشکلی پیش آمد',
+              icon:'error',
+              buttons:'تایید'
+            })
+          }
+        })
+      }
+    })
+  }
   return (
     <>
       <DataTable title="دوره ها">
@@ -41,7 +77,7 @@ export default function AdminCourses() {
                 </button>
               </td>
               <td>
-                <button type="button" class="btn btn-danger delete-btn">
+                <button type="button" class="btn btn-danger delete-btn" onClick={()=>{deleteCourse(course._id)}}>
                   حذف
                 </button>
               </td>
