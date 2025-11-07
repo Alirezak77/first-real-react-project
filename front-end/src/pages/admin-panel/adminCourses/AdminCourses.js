d418bb018bbace1a5c3afd0e2506799188cbd44f
Alirezak77 <kangavari9@gmail.com>
2025-11-07 01:59:00 -0800

    show all courses in admin panel

--- front-end/src/pages/admin-panel/adminCourses/AdminCourses.js
+++ front-end/src/pages/admin-panel/adminCourses/AdminCourses.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import DataTable from "../../../components/admin-components/DataTable/DataTable";
 
 export default function AdminCourses() {
+  const [courses , setCourses] = useState([])
+
+  useEffect(()=>{
+    const localStorageData= JSON.parse(localStorage.getItem('user'))
+    fetch(`http://localhost:4000/v1/courses`,{
+      headers:{ Authorization : `Bearer ${localStorageData}`} 
+    }).then(res=>res.json()).then(courses=>{
+      console.log(courses);
+      setCourses(courses)
+
+      
+    })
+  },[])
   return (
-    <div>AdminCourses</div>
-  )
+    <>
+      <DataTable title="دوره ها">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>عنوان</th>
+
+              <th>مبلغ</th>
+              <th>وضعیت</th>
+            </tr>
+          </thead>
+          <tbody>
+            {courses.map((course , index)=>{
+              return <tr>
+              <td>{index + 1}</td>
+              <td>{course.name}</td>
+              <td>{course.price ===0 ? 'رایگان' : course.price.toLocaleString()+' ' + 'تومان'}</td>
+              <td>{course.isComplete ===0 ? 'در حال برگذاری': 'تکمیل شده'}</td>
+              <td>
+                <button type="button" class="btn btn-primary edit-btn">
+                  ویرایش
+                </button>
+              </td>
+              <td>
+                <button type="button" class="btn btn-danger delete-btn">
+                  حذف
+                </button>
+              </td>
+            </tr>
+            })}
+            
+          </tbody>
+        </table>
+      </DataTable>
+    </>
+  );
 }
