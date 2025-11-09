7b5ce6ccb08e44a054103107c2777e25134dfa65
Alirezak77 <kangavari9@gmail.com>
2025-11-09 10:14:10 -0800

    show article list in admin panel

--- front-end/src/pages/admin-panel/adminArticle/AdminArticle.js
+++ front-end/src/pages/admin-panel/adminArticle/AdminArticle.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DataTable from '../../../components/admin-components/DataTable/DataTable'
 
 
 export default function AdminArticle() {
+  const [allArticle, setAllArticle]= useState([])
+  const localStorageData= JSON.parse(localStorage.getItem('user'))
+
+  useEffect(()=>{
+    fetch(`http://localhost:4000/v1/articles`,{
+      headers:{'Authorization' : `Bearer ${localStorageData}`}
+    })
+    .then(res=>res.json())
+    .then(result=>{
+      setAllArticle(result)
+    })
+  
+  },[])
   return (
-    <div id='content'>
-      
-    </div>
+    <>
+    <DataTable title={'مقاله ها'}>
+      <table class="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>عنوان مقاله</th>
+              <th>لینک</th>
+              <th>ویرایش</th>
+              <th>حذف</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allArticle.map((article, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{article.title}</td>
+                  <td>{article.shortName}</td>
+                  <td>
+                    <button type="button" class="btn btn-primary edit-btn">
+                      ویرایش
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-danger delete-btn"
+                    >
+                      حذف
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+    </DataTable>
+    </>
   )
 }
