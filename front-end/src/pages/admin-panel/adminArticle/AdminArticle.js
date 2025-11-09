02e14bcc6625e75c5da5616b8b7e681290eea2c1
Alirezak77 <kangavari9@gmail.com>
2025-11-09 10:51:37 -0800

    remove article in the admin panel

--- front-end/src/pages/admin-panel/adminArticle/AdminArticle.js
+++ front-end/src/pages/admin-panel/adminArticle/AdminArticle.js
@@ -1,25 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import DataTable from '../../../components/admin-components/DataTable/DataTable'
-
+import React, { useEffect, useState } from "react";
+import DataTable from "../../../components/admin-components/DataTable/DataTable";
+import swal from "sweetalert";
 
 export default function AdminArticle() {
-  const [allArticle, setAllArticle]= useState([])
-  const localStorageData= JSON.parse(localStorage.getItem('user'))
+  const [allArticle, setAllArticle] = useState([]);
+  const localStorageData = JSON.parse(localStorage.getItem("user"));
+  function getAllArticle() {
+    fetch(`http://localhost:4000/v1/articles`, {
+      headers: { Authorization: `Bearer ${localStorageData}` },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setAllArticle(result);
+      });
+  }
 
-  useEffect(()=>{
-    fetch(`http://localhost:4000/v1/articles`,{
-      headers:{'Authorization' : `Bearer ${localStorageData}`}
-    })
-    .then(res=>res.json())
-    .then(result=>{
-      setAllArticle(result)
-    })
-  
-  },[])
+  useEffect(() => {
+    getAllArticle();
+  }, []);
+
+  const removeArticle = (articleID) => {
+    swal({
+      title: "آیا از حذف مقاله اطمینان دارید؟",
+      icon: "warning",
+      buttons: ["نه", "بله"],
+    }).then((result) => {
+      if (result) {
+        fetch(`http://localhost:4000/v1/articles/${articleID}`, {
+          method: "DELETE",
+          headers: { Authorization: `Bearer ${localStorageData}` },
+        })
+          .then((res) => {
+            if (res.ok) {
+              swal({
+                title: "مقاله مورد نظر با موفقیت حذف شد",
+                icon: "success",
+                buttons: "تایید",
+              });
+            } else {
+              swal({
+                title: "در حذف مقاله مشکلی پیش آمد",
+                buttons: "تایید",
+              });
+            }
+          })
+          .then(() => {
+            getAllArticle();
+          });
+      }
+    });
+  };
   return (
     <>
-    <DataTable title={'مقاله ها'}>
-      <table class="table">
+      <DataTable title={"مقاله ها"}>
+        <table class="table">
           <thead>
             <tr>
               <th>شناسه</th>
@@ -45,6 +79,9 @@ export default function AdminArticle() {
                     <button
                       type="button"
                       class="btn btn-danger delete-btn"
+                      onClick={() => {
+                        removeArticle(article._id);
+                      }}
                     >
                       حذف
                     </button>
@@ -54,7 +91,7 @@ export default function AdminArticle() {
             })}
           </tbody>
         </table>
-    </DataTable>
+      </DataTable>
     </>
-  )
+  );
 }
