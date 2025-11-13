8b77dc96c9bd364b35b6ebdb9724b3ea1cc1fadc
Alirezak77 <kangavari9@gmail.com>
2025-11-13 01:26:44 -0800

    show menus in the admin panel

--- front-end/src/pages/admin-panel/menus/Menus.js
+++ front-end/src/pages/admin-panel/menus/Menus.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import DataTable from "../../../components/admin-components/DataTable/DataTable";
 
 export default function Menus() {
+  const [allMenu, setAllMenu] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/menus/all`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAllMenu(data);
+      });
+  }, []);
   return (
-    <div>Menus</div>
-  )
+    <>
+      <DataTable title={"منو ها"}>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>شناسه</th>
+              <th>عنوان</th>
+              <th>مقصد</th>
+              <th>زیر مجموعه</th>
+
+              <th>ویرایش</th>
+              <th>حذف</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allMenu.map((menu, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{menu.title}</td>
+                  <td>{menu.href}</td>
+                  <td>{menu.parent ? menu.parent.title : "منو اصلی"}</td>
+                  <td>
+                    <button type="button" class="btn btn-primary edit-btn">
+                      ویرایش
+                    </button>
+                  </td>
+                  <td>
+                    <button type="button" class="btn btn-danger delete-btn">
+                      حذف
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </DataTable>
+    </>
+  );
 }
