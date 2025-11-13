772f1bcba43cb4bb26b1bc78904f3e348403b5b0
Alirezak77 <kangavari9@gmail.com>
2025-11-13 02:09:58 -0800

    create new menu and sub menu in the admin panel

--- front-end/src/pages/admin-panel/menus/Menus.js
+++ front-end/src/pages/admin-panel/menus/Menus.js
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "../../../components/admin-components/DataTable/DataTable";
 import swal from "sweetalert";
+import { useForm } from "../../../hooks/useForm";
+import InpoutComponent from "../../../components/inpout-component/InpoutComponent";
+import { minValidator } from "../../../validators/rules";
 
 export default function Menus() {
   const [allMenu, setAllMenu] = useState([]);
+  const [menuParent , setMenuParent]=useState('-1')
   const localStorageData = JSON.parse(localStorage.getItem("user"));
+  const [formState , onInputHandler]=useForm({
+    title:{
+      value:'',
+      isValid:false
+    },
+    href:{
+      value:'',
+      isValid:false
+    }
 
+  },false)
+
+
+  //get all menu
   function getAllMenu() {
     fetch(`http://localhost:4000/v1/menus/all`)
       .then((res) => res.json())
@@ -13,10 +30,13 @@ export default function Menus() {
         setAllMenu(data);
       });
   }
+
   useEffect(() => {
     getAllMenu();
   }, []);
 
+
+  //delete menu
   const deleteMenu = (menuID) => {
     swal({
       title: "آیا از حذف منو اطمینان دارید؟",
@@ -41,8 +61,102 @@ export default function Menus() {
       }
     });
   };
+
+  //craete new menu
+  const createMenu = (event)=>{
+    event.preventDefault()
+
+    const menuInfo= {
+      title: formState.inputs.title.value,
+      href: formState.inputs.href.value,
+      parent: menuParent === '-1' ? undefined : menuParent
+    }
+
+    fetch(`http://localhost:4000/v1/menus`,{
+      method:'POST',
+      headers:{
+        Authorization:`Bearer ${localStorageData}`,
+        'Content-Type': 'application/json',
+
+      },
+      body: JSON.stringify(menuInfo)
+    }).then(res=>{
+      if(res.ok){
+        swal({
+          title:'منو مورد نظر با موفقیت اضافه شد',
+          icon:'success',
+          buttons:'تایید'
+        }).then(()=>{
+          getAllMenu()
+        })
+      }
+    })
+  }
   return (
     <>
+      <div class="container">
+        <div class="home-title">
+          <span>افزودن منو جدید</span>
+        </div>
+        <form class="form">
+          <div class="col-6">
+            <div class="name input">
+              <label class="input-title">عنوان</label>
+              <InpoutComponent
+                element="input"
+                onInputHandler={onInputHandler}
+                id="title"
+                type="text"
+                isValid="false"
+                placeholder="لطفا عنوان را وارد کنید..."
+                validations={[minValidator(5)]}
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="name input">
+              <label class="input-title">لینک</label>
+              <InpoutComponent
+                element="input"
+                onInputHandler={onInputHandler}
+                id="href"
+                type="text"
+                isValid="false"
+                validations={[minValidator(5)]}
+                placeholder="لطفا لینک را وارد کنید..."
+              />
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="name input">
+              <label class="input-title">منو اصلی</label>
+              <select
+                class="select"
+                onChange={(event) => setMenuParent(event.target.value)}
+              >
+                <option value="-1">منوی اصلی را انتخاب کنید</option>
+                {allMenu.map((menu) => (
+                  <>
+                    {!Boolean(menu.parent) && (
+                      <option value={menu._id}>{menu.title}</option>
+                    )}
+                  </>
+                ))}
+              </select>
+              <span class="error-message text-danger"></span>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="bottom-form">
+              <div class="submit-btn">
+                <input type="submit" value="افزودن" onClick={createMenu} />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
       <DataTable title={"منو ها"}>
         <table class="table">
           <thead>
