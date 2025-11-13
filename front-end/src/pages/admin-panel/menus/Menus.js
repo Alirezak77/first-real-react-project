63c5642943d3bd8402606624366126574175685e
Alirezak77 <kangavari9@gmail.com>
2025-11-13 01:36:05 -0800

    delete menu in the admin panel

--- front-end/src/pages/admin-panel/menus/Menus.js
+++ front-end/src/pages/admin-panel/menus/Menus.js
@@ -1,16 +1,46 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "../../../components/admin-components/DataTable/DataTable";
+import swal from "sweetalert";
 
 export default function Menus() {
   const [allMenu, setAllMenu] = useState([]);
-  useEffect(() => {
+  const localStorageData = JSON.parse(localStorage.getItem("user"));
+
+  function getAllMenu() {
     fetch(`http://localhost:4000/v1/menus/all`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setAllMenu(data);
       });
+  }
+  useEffect(() => {
+    getAllMenu();
   }, []);
+
+  const deleteMenu = (menuID) => {
+    swal({
+      title: "آیا از حذف منو اطمینان دارید؟",
+      icon: "warning",
+      buttons: ["خیر", "بله"],
+    }).then((result) => {
+      if (result) {
+        fetch(`http://localhost:4000/v1/menus/${menuID}`, {
+          method: "DELETE",
+          headers: { Authorization: `Bearer ${localStorageData}` },
+        }).then((res) => {
+          if (res.ok) {
+            swal({
+              title: "منو مورد نظر با موفقیت حذف شد",
+              icon: "success",
+              buttons: "تایید",
+            }).then(() => {
+              getAllMenu();
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <>
       <DataTable title={"منو ها"}>
@@ -40,7 +70,13 @@ export default function Menus() {
                     </button>
                   </td>
                   <td>
-                    <button type="button" class="btn btn-danger delete-btn">
+                    <button
+                      type="button"
+                      class="btn btn-danger delete-btn"
+                      onClick={() => {
+                        deleteMenu(menu._id);
+                      }}
+                    >
                       حذف
                     </button>
                   </td>
